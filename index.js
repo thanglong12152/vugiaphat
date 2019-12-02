b5ea9ac0f8fa9c98e8637a6749800edabba407fd
thanglong12152 <lamthanglong12152@gmail.com>
2019-12-02 23:56:10 +0700

    update

--- index.js
+++ index.js
@@ -27,4 +27,30 @@ $(document).ready(function() {
     $('#vd-ctx').css('z-index',3);
   });
 
-  
+  $(document).ready(function(){
+    $('#slideshow_slider').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
